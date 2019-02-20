149e07e707959faa4cc4c071f995397c775849b8
Matt Webb <matt531320.efreak@gmail.com>
2019-02-20 10:17:34 -0800

    Minor modifications for clarity

--- src/main/java/edu/csumb/spring19/capstone/config/Swagger.java
+++ src/main/java/edu/csumb/spring19/capstone/config/Swagger.java
@@ -1,10 +1,12 @@
 package edu.csumb.spring19.capstone.config;
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,11 +23,13 @@ public class Swagger {
         return new Docket(DocumentationType.SWAGGER_2)
               .select()
               .apis(RequestHandlerSelectors.basePackage("edu.csumb.spring19.capstone"))
-              .paths(PathSelectors.any())
+              .paths(Predicates.not(PathSelectors.regex("/error.*")))
               .build()
-              .securitySchemes(new ArrayList<>(Arrays.asList(
-                    new ApiKey("bearer %", "Authorization", "Header")
-              )));
+              .securitySchemes(securitySchemes());
+    }
+
+    private static ArrayList<? extends SecurityScheme> securitySchemes() {
+        return new ArrayList<>(Arrays.asList(new ApiKey("Bearer", "Authorization", "header")));
     }
 }
 
--- src/main/java/edu/csumb/spring19/capstone/controllers/UserController.java
+++ src/main/java/edu/csumb/spring19/capstone/controllers/UserController.java
@@ -4,6 +4,8 @@ import edu.csumb.spring19.capstone.dto.RestDTO;
 import edu.csumb.spring19.capstone.dto.RestData;
 import edu.csumb.spring19.capstone.models.PLUser;
 import edu.csumb.spring19.capstone.services.UserInterface;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,11 +15,14 @@ public class UserController {
     @Autowired
     private UserInterface userService;
 
+    @ApiOperation(value = "Sign in and issue a token for the user.")
     @GetMapping("/signin")
     public RestDTO signin(@RequestParam String username, @RequestParam String password) throws Exception {
         return new RestData<>(userService.signin(username, password));
     }
 
+    @ApiOperation(value = "List all user accounts.",
+          authorizations = {@Authorization(value = "Bearer")})
     @GetMapping("/userlist")
     public RestDTO userList() {
         return new RestData<Iterable<PLUser>>(userService.allUsers());
