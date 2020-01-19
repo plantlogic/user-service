7b3ef4a035ee21bd6cc79b67155a0bbc5f7f8972
ChristopherLeeWilliams <chris_lw16@yahoo.com>
2020-01-18 23:50:50 -0800

    added contractor role

--- src/main/java/edu/csumb/spring19/capstone/models/PLRole.java
+++ src/main/java/edu/csumb/spring19/capstone/models/PLRole.java
@@ -3,7 +3,7 @@ package edu.csumb.spring19.capstone.models;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum PLRole implements GrantedAuthority {
-    DATA_ENTRY, DATA_VIEW, DATA_EDIT, USER_MANAGEMENT, APP_ADMIN;
+    DATA_ENTRY, DATA_VIEW, DATA_EDIT, USER_MANAGEMENT, CONTRACTOR_VIEW, CONTRACTOR_EDIT, APP_ADMIN;
 
     @Override
     public String getAuthority() {
